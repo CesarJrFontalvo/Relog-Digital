d8a9537197c5467315bdcb3a59c807f6df2ae7e4
Cesar Jr.Fontalvo <juniorfontalvo5@gmail.com>
2020-10-26 17:00:55 -0500

    Se agregó una funcion para centrar pantalla

--- reloj.py
+++ reloj.py
@@ -1,6 +1,16 @@
 
 from  tkinter import *
 import time
+
+# Funcion para centrar la pantalla 
+def center(toplevel):
+       toplevel.update_idletasks()
+       w = toplevel.winfo_screenwidth()
+       h = toplevel.winfo_screenheight()
+       size = tuple(int(_) for _ in toplevel.geometry().split('+')[0].split('x'))
+       x = w/2 - size[0]/2
+       y = h/2 - size[1]/2
+       toplevel.geometry("%dx%d+%d+%d" % (size + (x, y)))
  
 #FUNCION PARA ACTUALIZAR LA HORA
 def times():
@@ -10,6 +20,7 @@ def times():
  
 #VENTANA
 root=Tk()
+center(root)
 root.geometry("485x250")
 root.title("Reloj digital con Tkinter")
 #root.configure(bg='#856ff8')
